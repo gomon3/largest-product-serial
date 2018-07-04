61ae4954f93f488b351362c0233127089cdbacf8
Brandon <bran_gomon25@hotmail.com>
2018-07-04 12:01:44 -0500

    Integración de parte de la interfaz gráfica y recuperamiento de t que denota el número de casos de prueba

--- src/largestproduct/FXMLMainController.java
+++ src/largestproduct/FXMLMainController.java
@@ -6,8 +6,15 @@
 package largestproduct;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 
 /**
  * FXML Controller class
@@ -19,6 +26,70 @@ public class FXMLMainController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Label remainingTestCases;
+    
+    
+    
+    private int t;
+    
+    @FXML
+    protected void addTestCase(ActionEvent e){
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Agregar caso de prueba");
+        dialog.setHeaderText("Agrega el número de casos de prueba que deseas ejecutar");
+        dialog.setContentText("Recuerda que el dato debe ser entre 1 y 100");
+        
+        
+        Optional<String> result = dialog.showAndWait();
+        
+        if(result.isPresent()){
+            
+            try{
+                
+                int t = Integer.parseInt(result.get());
+                
+                if(t<1 || t>100){
+                    
+                    showWarning("Debes ingresar un número entero entre 1 y 100");
+                    
+                }else{
+                    
+                    this.t = t;
+                    this.remainingTestCases.setText("Casos de prueba restantes: " + t);
+                    this.remainingTestCases.setVisible(true);
+                    
+                }
+                
+            }catch(Exception ex){
+                
+                showError("El dato ingresado no es válido");
+                
+            }
+            
+        }
+        
+    }
+    
+    private void showError(String mensaje){
+        
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Mensaje de error");
+        alert.setHeaderText("Ocurrió un error");
+        alert.setContentText(mensaje);
+
+        alert.showAndWait();
+    }
+    
+    private void showWarning(String mensaje){
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("Mensaje de advertencia");
+        alert.setHeaderText("Ocurrió un error");
+        alert.setContentText(mensaje);
+
+        alert.showAndWait();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
